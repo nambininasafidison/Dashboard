8fe633e88e087dbcdf2f07dd971d690eeec15ffa
NiainaTR <tsantaniainaraherison@gmail.com>
2024-03-18 01:16:31 +0300

    test data ' notif from file

--- menu-admin/script.js
+++ menu-admin/script.js
@@ -4,14 +4,37 @@ const optionProxy = document.querySelector('#option-proxy');
 const optionAcl = document.querySelector('#option-acl');
 const bell = document.querySelector('.bell');
 const containerNotif = document.querySelector('.container-notification');
+const allNotif = document.querySelector('.all-notif');
 
+/*
+<div class="notif">
+                <div class="notif-deco"></div>
+                <div class="notif-content">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+                <span>X</span>
+            </div>
+
+*/
 
 bell.addEventListener('click' , () => {
     console.log(containerNotif.style.opacity);
     if(containerNotif.style.opacity == 0){
         containerNotif.style.opacity = 1;
         bell.style.backgroundColor = "#FF0000";
-        bell.style.color = "#FFFFFF";                
+        bell.style.color = "#FFFFFF";
+        fetch('../data/data_notif.txt')
+        .then((response) => {
+            response.text()
+            .then((data) => {
+                const tabNotif = data.split('\n').reverse();
+                for(let line of tabNotif){
+                    const notif = line.split('|');
+                    console.log(notif);
+                }
+            })
+        })
+        .catch((err) => console.log("error fetching : " + err))
     }
     else{
         containerNotif.style.opacity = 0;
