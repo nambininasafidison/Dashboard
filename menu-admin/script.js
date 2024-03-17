9cffd5e782f4c65386ac1b893a50d6ed71df254c
NiainaTR <tsantaniainaraherison@gmail.com>
2024-03-18 01:15:57 +0300

    add interface notification

--- menu-admin/script.js
+++ menu-admin/script.js
@@ -2,6 +2,23 @@ const btnPrevious = document.querySelector('.btn-previous');
 const btnNext = document.querySelector('.btn-next');
 const optionProxy = document.querySelector('#option-proxy');
 const optionAcl = document.querySelector('#option-acl');
+const bell = document.querySelector('.bell');
+const containerNotif = document.querySelector('.container-notification');
+
+
+bell.addEventListener('click' , () => {
+    console.log(containerNotif.style.opacity);
+    if(containerNotif.style.opacity == 0){
+        containerNotif.style.opacity = 1;
+        bell.style.backgroundColor = "#FF0000";
+        bell.style.color = "#FFFFFF";                
+    }
+    else{
+        containerNotif.style.opacity = 0;
+        bell.style.backgroundColor = "#FFFFFF";
+        bell.style.color = "#1E1E1E";        
+    }
+})
 
 optionProxy.addEventListener('click' , () => console.log("optionProxy"));
 
